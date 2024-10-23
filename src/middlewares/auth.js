4c0ccc3637e82aa0fb00d94e50d533d057e38d7a
Deividokun <129647532+Deividokun@users.noreply.github.com>
2024-10-23 13:30:14 +0200

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -38,5 +38,27 @@ const Admin = async (req, res, next) => {
     return res.status(400).json('No puedes')
   }
 }
+const isOwnerOrAdmin = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const parsedToken = token.replace('Bearer ', '');
+    const { id } = verifyJwt(parsedToken); // Decodifica el token para obtener el ID del usuario
 
+    const user = await User.findById(id); 
+    if (!user) {
+      return res.status(404).json('Usuario no encontrado');
+    }
+
+    const userIdToModify = req.params.id; 
+  
+    if (user._id.toString() === userIdToModify || user.papel === 'admin') {
+      req.user = user; 
+      next();
+    } else {
+      return res.status(403).json('No tienes permisos para realizar esta acción');
+    }
+  } catch (error) {
+    return res.status(403).json('Error de autenticación');
+  }
+};
 module.exports = { Auth, Admin }
